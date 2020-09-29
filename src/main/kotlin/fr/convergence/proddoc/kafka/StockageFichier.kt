1b254e0b8a4dd5d8692350f4d0dd254ba2af64cc
AldoLaClasse <alain.meynier@gmail.com>
2020-09-29 11:27:22 +0200

    refactoring pour appeler Condor qui servira le Kbis à Stocker sur disque et en cache

--- src/main/kotlin/fr/convergence/proddoc/kafka/StockageFichier.kt
+++ src/main/kotlin/fr/convergence/proddoc/kafka/StockageFichier.kt
@@ -2,16 +2,21 @@ package fr.convergence.proddoc.kafka
 
 import fr.convergence.proddoc.model.lib.obj.MaskMessage
 import fr.convergence.proddoc.model.metier.FichierAccessible
-import fr.convergence.proddoc.model.metier.FichierEcrit
+import fr.convergence.proddoc.model.metier.StockageFichier
 import fr.convergence.proddoc.util.FichierCache
 import fr.convergence.proddoc.util.FichierCache.creeURLKbisLocale
 import fr.convergence.proddoc.util.maskIOHandler
 import io.vertx.core.logging.Logger
 import io.vertx.core.logging.LoggerFactory.getLogger
+import org.apache.commons.io.FileUtils
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.eclipse.microprofile.reactive.messaging.Outgoing
 import java.io.File
+import java.io.InputStream
+import java.util.*
 import javax.enterprise.context.ApplicationScoped
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE
 
 @ApplicationScoped
 class StockageFichier() {
@@ -21,31 +26,50 @@ class StockageFichier() {
     }
 
     /**
-     * si un MaskMessage arrive sur le topic "fichier" (Incoming) :
-     * 1) récupère dans le message : l'URL absolue, l'URL relative
+     * si un MaskMessage arrive sur le topic "STOCKER_FICHIER_DEMANDE" (Incoming) :
+     * 1) récupère dans le message : l'URL du fichier où le demander
      * 2) stocke le fichier dans le système de stockage (actuellement Map en mémoire)
      * 3) publie un message de type MaskMessage donnant l'URL d'accès au fichier
+     * sortie sur le topic "STOCKER_FICHIER_DEMANDE"
      *  ou bien "KO" + exception
      **/
 
-    @Incoming("fichiercache")
-    @Outgoing("fichiercache_fini")
+    @Incoming("stocker_fichier_demande")
+    @Outgoing("stocker_fichier_reponse")
     fun traitementEvenementReceptionFichierEcrit(message: MaskMessage): MaskMessage = maskIOHandler(message) {
 
         //@TODO ces requires sont à basculer dans le maskIOHadler
         requireNotNull(message.entete.typeDemande) { "message.entete.typeDemande est null" }
         requireNotNull(message.objetMetier) { "message.objectMetier est null" }
 
-        val fichierEcrit = message.recupererObjetMetier<FichierEcrit>()
-        val cheminCompletFichier = fichierEcrit.fichierURLAbs
+        val fichierEcrit = message.recupererObjetMetier<StockageFichier>()
         val identifiantFichier = fichierEcrit.idMetierFichier
-        LOG.debug("Réception évènement fichier écrit : $cheminCompletFichier sous l'identifiant $identifiantFichier")
+        val urlAbsFichier = fichierEcrit.fichierURLAbs
+        LOG.debug("Réception évènement demande de stockage : $urlAbsFichier sous l'identifiant $identifiantFichier")
 
+        val fichierastocker = message.recupererObjetMetier<StockageFichier>()
+        val inputStream = ClientBuilder.newClient()
+                .target(fichierastocker.fichierURLAbs)
+                .request(APPLICATION_OCTET_STREAM_TYPE)
+                .get(InputStream::class.java)
+        LOG.debug("fichier $identifiantFichier téléchargé")
+
+        // écrire fichier sur file system
+        val tempDir = System.getProperty("java.io.tmpdir")
+        LOG.debug("tempDir : $tempDir")
+        val cheminCompletFichier = "$tempDir/$identifiantFichier${UUID.randomUUID()}.fic"
+
+        FileUtils.copyInputStreamToFile(inputStream, File(cheminCompletFichier))
+        LOG.debug("fichier $cheminCompletFichier écrit sur file system")
+
+        // déposer fichier dans cache
         val fichier = File(cheminCompletFichier)
         FichierCache.deposeFichierCache(fichier, identifiantFichier,)
+        LOG.debug("fichier $identifiantFichier déposé dans cache local")
+
         val urlFichier = creeURLKbisLocale(identifiantFichier)
         LOG.debug("URL du fichier accessible sur Stinger : $urlFichier")
-
         FichierAccessible(urlFichier)
+
     }
 }
